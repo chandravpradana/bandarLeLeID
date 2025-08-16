6f3f93106c48f3f1c5b7d95ff1aeace2b1da29fe
chandravpradana <chandravitoncapradana@gmail.com>
2025-08-16 14:49:57 +0700

    Delete bandarLeLeID directory

--- bandarLeLeID/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initTheme } from './theme.js';
-import { initForm } from './form.js';
-import { updateStats } from './stats.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initTheme();
-    initForm();
-    updateStats();
-});
